b8168fdf32ea8404973a69d2e3da42f55c3b8793
Noun <sophy.noun@covered.ca.gov>
2019-06-19 14:43:37 -0700

    Udemy Folders

---
+++ Documents/Udemy/Ex_125/script.js
@@ -0,0 +1,26 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+
+function inputLength() {
+    return input.value.length;
+}
+function addListAfterClick() {
+        if (inputLength() > 0) {
+            createListElements();
+        }
+}
+function addListAfterKeypress(event){
+        if (inputLength() > 0 && event.keyCode === 13) {
+            createListElement();
+        }
+}
+function createListElement() {
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
+}
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
